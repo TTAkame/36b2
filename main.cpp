0a35fa312212076d04d06b6a70c7ef92c64c33ab
TTAkame <113761684+TTAkame@users.noreply.github.com>
2022-10-25 18:03:17 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+
+#include "allclass.h"
+using namespace std;
+
+int main(){
+  
+    Person red;
+    Person mom;
+    Person gm;
+    Person wf;
+  //Little Red Ridding Hood wore the Cap
+    
+  //Her mom gave her the cake and wine
+    
+  //Little Red met the wolf at a particular spot in the wood
+    
+  //Grandma was at her own home right outside of the wood and there were three large oak trees nearby
+    
+    
+    return 0;
+}
