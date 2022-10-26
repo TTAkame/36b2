deb770acfbe0fbbbe5aae186604498852ec0aa08
TTAkame <113761684+TTAkame@users.noreply.github.com>
2022-10-25 18:26:44 -0700

    Update allclass.cpp

--- allclass.cpp
+++ allclass.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
 
+using namespace std;
+
+class Person{
+  public:
+    char name[10];
+
+}
+
+class Thing{
+  public:
+    char name[10];
+  
+}
+
+class Place{
+  public:
+    char name[30];
+    int glat;
+    int glon;
+}
+
+class
