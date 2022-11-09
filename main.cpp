b8a629cc8a69655ea52b7d28b8689ad923c7faa5
TTAkame <113761684+TTAkame@users.noreply.github.com>
2022-11-09 15:09:14 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,58 @@
-
 #include<iostream>
+
+#include <stdio.h>
+#include <iomanip>
+#include <cstdlib>
 #include <json/json.h>
+#include <json/reader.h>
+#include <json/writer.h>
+#include <json/value.h>
 
 
-#include "allclass.h"
+#include "tet.h"
 using namespace std;
 
 int main(){
   
-    Person red;
-    Person mom;
-    Person gm;
-    Person wf;
+ 
     
-    Thing cap;
-    Thing wine;
-    Thing cake;
-    Thing wine;
-    Thing oak;
-    
-    Place spot;
-    Place wood;
-    Place home;
-    Place toak;
-    
-    Time one;
+ /*   Time one;
     Time two;
     Time three;
-    Time four;
+    Time four; */
     
+    Person red{"Little Red"};
+    Person mom{"Mom"};
+    Person gra{"Grandma"};
+    Person wol{"wolf"};
+    
+    Thing cap{"cap"};
+    Thing wine{"wine"};
+    Thing cake{"cake"};
+    Thing oak{"oak"};
+
+    Place iw{"a spot in the wood"};
+    Place ow{"outside of the wood"};
+    Place ot{"nearby three large oak trees"};
+    Place hm{"home"};
+    
+    cout << (hm.dump2json()).toStyledString() << std::endl;
+/*
+
   //Little Red Ridding Hood wore the Cap
+  Record s1;
   
-  red.name = "Little Red Ridding Hood";
-  mom.name = "Her Mom";
-  gm.name = "Grandma";
-  wf.name = "Wolf";
-    
   //Her mom gave her the cake and wine
-    
-  
+  Record s2;
   
   //Little Red met the wolf at a particular spot in the wood
+  Record s3;
     
   //Grandma was at her own home right outside of the wood and there were three large oak trees nearby
+  Record s4;
     
-    
+*/
     return 0;
 }
+
+//std::cout << (m.dump2json()).toStyledString() << std::endl;
